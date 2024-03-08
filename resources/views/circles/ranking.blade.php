05af918051cb67940baed4e86faa675f50a8d668
Shota Sato <last11142@gmail.com>
2024-03-09 00:20:35 +0900

    feat: #3 ランキングのルートを定義

---
+++ resources/views/circles/ranking.blade.php
@@ -0,0 +1,1 @@
+<div>circles.ranking</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CircleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('circles')  // URLルート circles/ に対応
+->controller(CircleController::class)  // このルートグループの全てのルートが CircleController を使用する
+->name('circles.')  // ルートの名前に circles. のプレフィクスを設定
+->group(function() {
+    Route::get('/ranking', 'ranking')->name('ranking');  // URLパス circles/ranking に対して CircleController.ranking にルーティングする。このルートに circles.ranking の名前を指定する
 });
