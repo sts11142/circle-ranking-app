959693f1131d254dbae46fd7406469737cd2fd56
Shota Sato <last11142@gmail.com>
2024-03-13 23:14:25 +0900

    feat: #9 カテゴリカラムを追加する

---
+++ database/migrations/2024_03_13_230346_add_circle_category_to_circles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('circles', function (Blueprint $table) {
+            $table->enum('circle_category', [
+                'sports',
+                'cultural',
+                'group',
+                'committee',
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('circles', function (Blueprint $table) {
+            $table->dropColumn('circle_category');
+        });
+    }
+};
